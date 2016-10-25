40c5ab6407c80d3b162e64c4accf830d2165da3d
Octavian Ionescu <ocio@netop.com>
2016-10-25 17:53:14 +0300

    finished rebuild for docker compose
    Signed-off-by: Octavian Ionescu <ocio@netop.com>

--- README.md
+++ README.md
@@ -19,6 +19,24 @@
 `git clone git@git.netop.com:portal/portal-infrastructure.git`  
 `cd portal-infrastructure`
 
+## install with docker-compose
+* you must have installed docker-compose support
+    * if docker-compose command is njotin you path edit file `docker-compose.env` and set the var `COMPOSE_COMMAND` with full path command for docker compose 
+* run `./install-compose.sh <param1> (<param2>)`
+    * <param1> is full path to folder created earlier (mkdir netop)
+    * <param2> is optional; is the name of the service you want to rebuild:
+        * netop-cache
+        * netop-db
+        * netop-mq
+        * netop-web
+        * netop-portal
+        * netop-nas
+        * netop-permissions
+
+<br>
+<br>
+<br>
+## install without docker-compose
 * run `./install.sh <param1> <param2>`
     * <param1> is full path to folder created earlier (mkdir netop)
     * <param2> is the ip of the docker engine interface (on linux it is usually `docker0` interface)
@@ -27,7 +45,13 @@
 `docker start netop-portal`
 `docker start netop-nas`
 `docker start netop-permissions`
-* edit your hosts file & add the following lines (replacing the IP with your docker-ip - <param2> for the install.sh) :  
+
+<br>
+<br>
+<br>
+## usage
+
+* edit your hosts file & add the following lines (replacing the IP with your docker-ip) :  
 `<docker interface ip>	portal-local.netop.com nas-local.netop.com`
 * navigate to http://portal-local.netop.com
 * login using:  
@@ -35,6 +59,8 @@ user:`admin@netop.com`
 pass:`123456`
 * if you need to run a command inside a docker use following command:
 `docker exec -it (<dockerid>|<docker-name>) /usr/local/bin/su-exec "$(id -un)"  /bin/bash`
+or (if you used docker command)
+`docker-compose exec <service-name> /usr/local/bin/su-exec itavy /bin/bash`
 * for each project it will create 3 distinct folders:
     * config
         * it will contain a special file app.env in which are defined env variables needed for the process to run
@@ -49,7 +75,9 @@ pass:`123456`
         * all the changes regarding the behaviour of the process are made here, ex:
             * change a file
             * switch another branch
-<br />
-<br />
+
+<br>
+<br>
+<br>
 ## IMPORTANT NOTE:
 when you make a change in any of the following projects: netop-portal, netop-nas or netop-permissions you must manually restart the coresponding container so that the changes take effect 
--- install-compose.sh
+++ install-compose.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
-if [ $# -ne 1 ]; then
+# docker compose tags
+COMPOSE_CACHE="netop-cache";
+COMPOSE_DB="netop-db";
+COMPOSE_MQ="netop-mq";
+COMPOSE_WEB="netop-web";
+
+COMPOSE_PORTAL="netop-portal";
+COMPOSE_NAS="netop-nas";
+COMPOSE_PERMISSIONS="netop-permissions";
+
+if [ $# -lt 1 ]; then
   echo "invalid number of arguments";
   echo "first parameter must be full path to projects folder";
+  echo "second is optional and can be the name of the service you want to rebuild:"
+  echo "$COMPOSE_CACHE";
+  echo "$COMPOSE_DB";
+  echo "$COMPOSE_MQ";
+  echo "$COMPOSE_WEB";
+  echo "$COMPOSE_PORTAL";
+  echo "$COMPOSE_NAS";
+  echo "$COMPOSE_PERMISSIONS";
   exit 2;
 fi
 
@@ -15,30 +33,16 @@ else
   exit 2;
 fi
 
-echo "Login to netop docker registry using domain credentials:"
-docker login git.netop.com:4545 || exit 1;
-
-echo "Trying to get latest image for developer";
-docker pull git.netop.com:4545/portal/portal-docker-images:developer_F24_N0.12.7_PB3.0.2 || exit 1;
-
-
 CURRENT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
 
 source "$CURRENT_DIRECTORY/docker-compose.env";
 
-# docker compose tags
-COMPOSE_CACHE="netop-cache";
-COMPOSE_DB="netop-db";
-COMPOSE_MQ="netop-mq";
-COMPOSE_WEB="netop-web";
-
-COMPOSE_PORTAL="netop-portal";
-COMPOSE_NAS="netop-nas";
-COMPOSE_PERMISSIONS="netop-permissions";
-
 # variables for install setup
 INSTALL_DIR="$1" # ex "/home/itavy/tmp/transfer/test2";
-# DOCKER_IP="$2" # ex "172.17.0.1";
+BUILD_OPTION="all";
+if [ $# -gt 1 ]; then
+    BUILD_OPTION=$2;
+fi
 
 
 # user details for folders mappings
@@ -47,152 +51,289 @@ NETOP_USER_ID=`id -u`;
 NETOP_USER_GROUP_ID=`id -g $NETOP_USER_NAME`;
 NETOP_USER_GROUP_NAME=`id -gn $NETOP_USER_NAME`;
 
+doLocalImageForDevelopment () {
+    echo "Login to netop docker registry using domain credentials:"
+    docker login git.netop.com:4545 || exit 1;
 
-if [ -f Dockerfile.localDeveloper ]; then
-  rm -rf Dockerfile.localDeveloper;
-fi
+    echo "Trying to get latest image for developer";
+    docker pull git.netop.com:4545/portal/portal-docker-images:developer_F24_N0.12.7_PB3.0.2 || exit 1;
 
-if [ "$NETOP_USER_ID" == "0" ]; then
-  cp Dockerfile.root.localDeveloper Dockerfile.localDeveloper;
-else
-  cp Dockerfile.netopDeveloper Dockerfile.localDeveloper;
-  sed -i$SED_COMPLETION "s/<user_name>/$NETOP_USER_NAME/g" Dockerfile.localDeveloper;
-  sed -i$SED_COMPLETION "s/<group_id>/$NETOP_USER_GROUP_ID/g" Dockerfile.localDeveloper;
-  sed -i$SED_COMPLETION "s/<group_name>/$NETOP_USER_GROUP_NAME/g" Dockerfile.localDeveloper;
-  sed -i$SED_COMPLETION "s/<user_id>/$NETOP_USER_ID/g" Dockerfile.localDeveloper;
-fi
+    cd "$CURRENT_DIRECTORY";
+    if [ -f Dockerfile.localDeveloper ]; then
+      rm -rf Dockerfile.localDeveloper;
+    fi
 
-docker build -t netop_local_develop -f Dockerfile.localDeveloper .
+    if [ "$NETOP_USER_ID" == "0" ]; then
+      cp Dockerfile.root.localDeveloper Dockerfile.localDeveloper;
+    else
+      cp Dockerfile.netopDeveloper Dockerfile.localDeveloper;
+      sed -i$SED_COMPLETION "s/<user_name>/$NETOP_USER_NAME/g" Dockerfile.localDeveloper;
+      sed -i$SED_COMPLETION "s/<group_id>/$NETOP_USER_GROUP_ID/g" Dockerfile.localDeveloper;
+      sed -i$SED_COMPLETION "s/<group_name>/$NETOP_USER_GROUP_NAME/g" Dockerfile.localDeveloper;
+      sed -i$SED_COMPLETION "s/<user_id>/$NETOP_USER_ID/g" Dockerfile.localDeveloper;
+    fi
 
-mkdir -p "$INSTALL_DIR"/{portal,nas,weblogs,permissions};
-mkdir -p "$INSTALL_DIR"/portal/{logs,config/env};
-mkdir -p "$INSTALL_DIR"/nas/{logs,config/env};
-mkdir -p "$INSTALL_DIR"/permissions/{logs,config};
-mkdir -p "$INSTALL_DIR"/database;
+    docker build -t netop_local_develop -f Dockerfile.localDeveloper .
+    cd "$CURRENT_DIRECTORY";
+}
 
-cp confs/portal/app.env "$INSTALL_DIR"/portal/config/
-cp confs/portal/{apikeys.json,env-config.json,production.js} "$INSTALL_DIR"/portal/config/env/
+doDockerComposeFile () {
+    cd "$CURRENT_DIRECTORY";
+    if [ -f docker-compose.yml ]; then
+      rm -rf docker-compose.yml;
+    fi
+    cp docker-compose.yml.netopDeveloper docker-compose.yml
+    SED_FILE="$CURRENT_DIRECTORY"/docker-compose.yml;
 
-cp confs/nas/app.env "$INSTALL_DIR"/nas/config/
-cp confs/nas/{env-config.js,production.js} "$INSTALL_DIR"/nas/config/env/
+    sed -i$SED_COMPLETION "s#netop_portal_install_dir_placeholder#$INSTALL_DIR#g" "$SED_FILE";
+    cd "$CURRENT_DIRECTORY";
+}
 
-cp confs/permission/app.env "$INSTALL_DIR"/permissions/config/
+doPortalWorker ()
+{
+    echo "Building Netop Portal worker environment";
+    cd "$CURRENT_DIRECTORY";
+    if [ ! -d "$INSTALL_DIR"/portal/config ]; then
+        mkdir -p "$INSTALL_DIR"/portal/config/env;
+        cp confs/portal/app.env "$INSTALL_DIR"/portal/config/
+        cp confs/portal/{apikeys.json,env-config.json,production.js} "$INSTALL_DIR"/portal/config/env/
+
+        #production file for portal
+        SED_FILE="$INSTALL_DIR"/portal/config/env/production.js
+        sed -i$SED_COMPLETION "s/host: 'localhost',/host: '$COMPOSE_CACHE',/" "$SED_FILE"
+        sed -i$SED_COMPLETION "s/'localhost:6379'/'$COMPOSE_CACHE:6379'/" "$SED_FILE"
+
+        #app.env for portal
+        SED_FILE="$INSTALL_DIR"/portal/config/app.env
+        sed -i$SED_COMPLETION "s/<rabbitmqhost>:<rabbitmqport>\/<rabbitmqvhost>/$COMPOSE_MQ\/netop-local/" "$SED_FILE";
+        sed -i$SED_COMPLETION "s/<redishost>/$COMPOSE_CACHE/" "$SED_FILE";
+        sed -i$SED_COMPLETION "s/mysql:\/\/root:dev@<host>\/portal/mysql:\/\/root:dev@$COMPOSE_DB\/portal/" "$SED_FILE";
+
+        sed -i$SED_COMPLETION "s/<_nasip_>/d" "$SED_FILE";
+        sed -i$SED_COMPLETION "s/NAS_HOST/d" "$SED_FILE";
+        sed -i$SED_COMPLETION "s/^fi/d" "$SED_FILE";
+    fi
+
+    if [ ! -d "$INSTALL_DIR"/portal/logs ]; then
+        mkdir -p "$INSTALL_DIR"/portal/logs;
+    fi
+
+    if [ ! -d "$INSTALL_DIR"/portal/project ]; then
+        git clone git@git.netop.com:portal/netop-portal-server.git "$INSTALL_DIR"/portal/project;
+        cd "$INSTALL_DIR"/portal/project;
+        git checkout develop;
+    fi
+    cd "$CURRENT_DIRECTORY";
+
+    "$COMPOSE_COMMAND" up -d "$COMPOSE_PORTAL" || exit 1;
+
+    echo "install portal dependencies"
+    "$COMPOSE_COMMAND" exec "$COMPOSE_PORTAL" /usr/local/bin/su-exec $NETOP_USER_NAME /bin/sh -c "cd /netop-worker/files && /usr/local/bin/npm install && /usr/local/bin/npm run postinstall";
+    echo "finish install portal dependencies";
+
+    # restart needed only at first install when there are no dependencies installed
+    "$COMPOSE_COMMAND" restart "$COMPOSE_PORTAL"
+    cd "$CURRENT_DIRECTORY";
+}
+
+doNasWorker ()
+{
+    echo "Build Netop NAS worker environment";
+    cd "$CURRENT_DIRECTORY";
+    if [ ! -d "$INSTALL_DIR"/nas/logs ]; then
+        mkdir -p "$INSTALL_DIR"/nas/logs;
+    fi
+
+    if [ ! -d "$INSTALL_DIR"/nas/config ]; then
+        mkdir -p "$INSTALL_DIR"/nas/config;
+        cp confs/nas/app.env "$INSTALL_DIR"/nas/config/;
+        cp confs/nas/{env-config.js,production.js} "$INSTALL_DIR"/nas/config/env/;
+
+        SED_FILE="$INSTALL_DIR"/nas/config/app.env
+        sed -i$SED_COMPLETION "s/<rabbitmqhost>:<rabbitmqport>\/<rabbitmqvhost>/$COMPOSE_MQ\/netop-local/" "$SED_FILE";
+        sed -i$SED_COMPLETION "s/<redis_host>/$COMPOSE_CACHE/" "$SED_FILE";
+        sed -i$SED_COMPLETION "s/<db_host>/$COMPOSE_DB/" "$SED_FILE";
+    fi
+
+    if [ ! -d "$INSTALL_DIR"/nas/project ]; then
+        git clone git@git.netop.com:portal/netop-nas.git "$INSTALL_DIR"/nas/project;
+        cd "$INSTALL_DIR"/nas/project;
+        git checkout develop;
+    fi
+    cd "$CURRENT_DIRECTORY";
+    "$COMPOSE_COMMAND" up -d "$COMPOSE_NAS" || exit 1;
+
+    echo "install nas dependencies"
+    "$COMPOSE_COMMAND" exec "$COMPOSE_NAS" /usr/local/bin/su-exec $NETOP_USER_NAME /bin/sh -c "cd /netop-worker/files && /usr/local/bin/npm install && /usr/local/bin/npm run postinstall";
+    echo "finish nas dependencies"
+
+    # restart needed only at first install when there are no dependencies installed
+    "$COMPOSE_COMMAND" restart "$COMPOSE_NAS"
+    cd "$CURRENT_DIRECTORY";
+}
+
+doPermissionsWorker ()
+{
+    echo "Build Permissions worker environment"
+    cd "$CURRENT_DIRECTORY";
+    if [ ! -d "$INSTALL_DIR"/permissions/logs ]; then
+        mkdir -p "$INSTALL_DIR"/permissions/logs;
+    fi
+
+    if [ ! -d "$INSTALL_DIR"/permissions/config ]; then
+        mkdir -p "$INSTALL_DIR"/permissions/config;
+        cp confs/permission/app.env "$INSTALL_DIR"/permissions/config/
+
+        #app.env for permissions
+        SED_FILE="$INSTALL_DIR"/permissions/config/app.env
+        sed -i$SED_COMPLETION "s/<rabbitmqhost>:<rabbitmqport>\/<rabbitmqvhost>/$COMPOSE_MQ\/netop-local/" "$SED_FILE";
+        sed -i$SED_COMPLETION "s/<redis_host>/$COMPOSE_CACHE/" "$SED_FILE";
+    fi
+
+    if [ ! -d "$INSTALL_DIR"/permissions/project ]; then
+        git clone git@git.netop.com:portal/netop-permissions.git "$INSTALL_DIR"/permissions/project;
+        cd "$INSTALL_DIR"/permissions/project;
+        git checkout develop;
+    fi
+
+    cd "$CURRENT_DIRECTORY";
+    "$COMPOSE_COMMAND" up -d "$COMPOSE_PERMISSIONS" || exit 1;
+    echo "install permissions dependencies"
+    "$COMPOSE_COMMAND" exec "$COMPOSE_PERMISSIONS" /usr/local/bin/su-exec $NETOP_USER_NAME /bin/sh -c "cd /netop-worker/files && /usr/local/bin/npm install";
+    echo "finish permissions dependencies"
+
+    # restart needed only at first install when there are no dependencies installed
+    "$COMPOSE_COMMAND" restart "$COMPOSE_PERMISSIONS"
+    cd "$CURRENT_DIRECTORY";
+}
+
+doNetopDatabase ()
+{
+    echo "Build Netop Databse Service";
+    cd "$CURRENT_DIRECTORY";
+    if [ ! -d "$INSTALL_DIR"/database ]; then
+        mkdir -p "$INSTALL_DIR"/database;
+    fi
+    "$COMPOSE_COMMAND" up -d "$COMPOSE_DB" || exit 1;
+    cd "$CURRENT_DIRECTORY";
+}
+
+doNetopCache ()
+{
+    echo "Build Netop Cache Service";
+    cd "$CURRENT_DIRECTORY";
+    "$COMPOSE_COMMAND" up -d "$COMPOSE_CACHE" || exit 1;
+}
+
+doNetopMq ()
+{
+    echo "Build Netop MQ Service";
+    cd "$CURRENT_DIRECTORY";
+    "$COMPOSE_COMMAND" up -d "$COMPOSE_MQ" || exit 1;
+}
 
 
+doNetopWeb ()
+{
+    echo "Build Netop WebFrontService";
+    cd "$CURRENT_DIRECTORY";
+    if [ ! -d "$INSTALL_DIR"/nas-frontend ]; then
+        git clone git@git.netop.com:portal/netop-nas-frontend.git "$INSTALL_DIR"/nas-frontend;
+        cd "$INSTALL_DIR"/nas-frontend;
+        git checkout develop;
+        npm install && npm run build;
+    fi
+    if [ ! -d "$INSTALL_DIR"/portal-frontend ]; then
+        git clone git@git.netop.com:portal/netop-portal-frontend.git "$INSTALL_DIR"/portal-frontend;
+        cd "$INSTALL_DIR"/portal-frontend;
+        git checkout develop;
+        npm install && npm run build;
+    fi
 
-#production file for portal
-SED_FILE="$INSTALL_DIR"/portal/config/env/production.js
-sed -i$SED_COMPLETION "s/host: 'localhost',/host: '$COMPOSE_CACHE',/" "$SED_FILE"
-sed -i$SED_COMPLETION "s/'localhost:6379'/'$COMPOSE_CACHE:6379'/" "$SED_FILE"
+    if [ ! -d "$INSTALL_DIR"/weblogs ]; then
+        mkdir "$INSTALL_DIR"/weblogs;
+    fi
+    cd "$CURRENT_DIRECTORY"/nginx;
+    if [ -d build ]; then
+      rm -rf build;
+    fi
+    mkdir build;
+    cp nginx.conf build/
+    cp portalapi.netop.com.conf build/
+    cp -R cert build/
 
-#app.env for portal
-SED_FILE="$INSTALL_DIR"/portal/config/app.env
-sed -i$SED_COMPLETION "s/<rabbitmqhost>:<rabbitmqport>\/<rabbitmqvhost>/$COMPOSE_MQ\/netop-local/" "$SED_FILE";
-sed -i$SED_COMPLETION "s/<redishost>/$COMPOSE_CACHE/" "$SED_FILE";
-sed -i$SED_COMPLETION "s/mysql:\/\/root:dev@<host>\/portal/mysql:\/\/root:dev@$COMPOSE_DB\/portal/" "$SED_FILE";
+    sed -i$SED_COMPLETION "s/<dockerip-portal>/$COMPOSE_PORTAL/g" build/portalapi.netop.com.conf;
+    sed -i$SED_COMPLETION "s/<dockerip-nas>/$COMPOSE_NAS/g" build/portalapi.netop.com.conf;
+    if [ -f build.tar.gz ]; then
+      rm -rf build.tar.gz
+    fi
+    tar czvf build.tar.gz build
 
-sed -i$SED_COMPLETION "s/<_nasip_>/d" "$SED_FILE";
-sed -i$SED_COMPLETION "s/NAS_HOST/d" "$SED_FILE";
-sed -i$SED_COMPLETION "s/^fi/d" "$SED_FILE";
+    cd "$CURRENT_DIRECTORY";
 
+    "$COMPOSE_COMMAND" up -d "$COMPOSE_WEB" || exit 1;
+    rm -rf "$CURRENT_DIRECTORY"/nginx/build "$CURRENT_DIRECTORY"/nginx/build.tar.gz
+    cd "$CURRENT_DIRECTORY";
+}
 
-#app.env for nas
-SED_FILE="$INSTALL_DIR"/nas/config/app.env
-sed -i$SED_COMPLETION "s/<rabbitmqhost>:<rabbitmqport>\/<rabbitmqvhost>/$COMPOSE_MQ\/netop-local/" "$SED_FILE";
-sed -i$SED_COMPLETION "s/<redis_host>/$COMPOSE_CACHE/" "$SED_FILE";
-sed -i$SED_COMPLETION "s/<db_host>/$COMPOSE_DB/" "$SED_FILE";
+doShowSuccessFinal ()
+{
+    echo "You must add following line to /etc/hosts:"
+    echo "<ip docker-engine interface>  nas-local.netop.com portal-local.netop.com";
+    echo ""
+    echo ""
+    echo "if you need to run a command inside docker use the following command:"
+    echo "docker exec -it (<dockerid>|<docker-name>) /usr/local/bin/su-exec $NETOP_USER_NAME /bin/bash";
+    echo "or"
+    echo "$COMPOSE_COMMAND exec (<service-name>) /usr/local/bin/su-exec $NETOP_USER_NAME /bin/bash";
+}
 
-#app.env for permissions
-SED_FILE="$INSTALL_DIR"/permissions/config/app.env
-sed -i$SED_COMPLETION "s/<rabbitmqhost>:<rabbitmqport>\/<rabbitmqvhost>/$COMPOSE_MQ\/netop-local/" "$SED_FILE";
-sed -i$SED_COMPLETION "s/<redis_host>/$COMPOSE_CACHE/" "$SED_FILE";
-
-#docker compose setup
-cd "$CURRENT_DIRECTORY";
-if [ -f docker-compose.yml ]; then
-  rm -rf docker-compose.yml;
-fi
-cp docker-compose.yml.netopDeveloper docker-compose.yml
-SED_FILE="$CURRENT_DIRECTORY"/docker-compose.yml;
-
-sed -i$SED_COMPLETION "s#netop_portal_install_dir_placeholder#$INSTALL_DIR#g" "$SED_FILE";
-
-#nginx requirements
-cd nginx;
-if [ -d build ]; then
-  rm -rf build;
-fi
-mkdir build;
-cp Dockerfile.nginx build/
-cp nginx.conf build/
-cp portalapi.netop.com.conf build/
-cp -R cert build/
-
-sed -i$SED_COMPLETION "s/<dockerip-portal>/$COMPOSE_PORTAL/g" build/portalapi.netop.com.conf;
-sed -i$SED_COMPLETION "s/<dockerip-nas>/$COMPOSE_NAS/g" build/portalapi.netop.com.conf;
-if [ -f build.tar.gz ]; then
-  rm -rf build.tar.gz
-fi
-tar czvf build.tar.gz build
-
-#git projects
-git clone git@git.netop.com:portal/netop-portal-frontend.git "$INSTALL_DIR"/portal-frontend;
-cd "$INSTALL_DIR"/portal-frontend;
-git checkout develop;
-npm install && npm run build;
-
-git clone git@git.netop.com:portal/netop-nas-frontend.git "$INSTALL_DIR"/nas-frontend;
-cd "$INSTALL_DIR"/nas-frontend;
-git checkout develop;
-npm install && npm run build;
-
-git clone git@git.netop.com:portal/netop-portal-server.git "$INSTALL_DIR"/portal/project;
-cd "$INSTALL_DIR"/portal/project;
-git checkout develop;
-git clone git@git.netop.com:portal/netop-nas.git "$INSTALL_DIR"/nas/project;
-cd "$INSTALL_DIR"/nas/project;
-git checkout develop;
-git clone git@git.netop.com:portal/netop-permissions.git "$INSTALL_DIR"/permissions/project;
-cd "$INSTALL_DIR"/permissions/project;
-git checkout develop;
-
-cd "$CURRENT_DIRECTORY";
-#chown -R $NETOP_USER_NAME. $INSTALL_DIR/{portal,nas,portal,nas-frontend,portal-frontend,permissions};
-
-
-"$COMPOSE_COMMAND" up -d "$COMPOSE_DB" || exit 1;
-"$COMPOSE_COMMAND" up -d "$COMPOSE_CACHE" || exit 1;
-"$COMPOSE_COMMAND" up -d "$COMPOSE_MQ" || exit 1;
-
-"$COMPOSE_COMMAND" up -d "$COMPOSE_PORTAL" || exit 1;
-"$COMPOSE_COMMAND" up -d "$COMPOSE_NAS" || exit 1;
-"$COMPOSE_COMMAND" up -d "$COMPOSE_PERMISSIONS" || exit 1;
-
-"$COMPOSE_COMMAND" up -d "$COMPOSE_WEB" || exit 1;
-
-rm -rf "$CURRENT_DIRECTORY"/nginx/build "$CURRENT_DIRECTORY"/nginx/build.tar.gz
-
-echo "install portal dependencies"
-"$COMPOSE_COMMAND" exec netop-portal /usr/local/bin/su-exec $NETOP_USER_NAME /bin/sh -c "cd /netop-worker/files && /usr/local/bin/npm install && /usr/local/bin/npm run postinstall";
-echo "finish install portal dependencies"
-
-echo "install nas dependencies"
-"$COMPOSE_COMMAND" exec netop-nas /usr/local/bin/su-exec $NETOP_USER_NAME /bin/sh -c "cd /netop-worker/files && /usr/local/bin/npm install && /usr/local/bin/npm run postinstall";
-echo "finish nas dependencies"
-
-echo "install permissions dependencies"
-"$COMPOSE_COMMAND" exec netop-permissions /usr/local/bin/su-exec $NETOP_USER_NAME /bin/sh -c "cd /netop-worker/files && /usr/local/bin/npm install";
-echo "finish permissions dependencies"
-
-"$COMPOSE_COMMAND" restart netop-nas
-"$COMPOSE_COMMAND" restart netop-portal
-"$COMPOSE_COMMAND" restart netop-permissions
-
-echo "You must add following line to /etc/hosts:"
-echo "<ip docker-engine interface>  nas-local.netop.com portal-local.netop.com";
-echo ""
-echo ""
-echo "if you need to run a command inside docker use the following command:"
-echo "docker exec -it (<dockerid>|<docker-name>) /usr/local/bin/su-exec $NETOP_USER_NAME /bin/bash";
-echo "or"
-echo "$COMPOSE_COMMAND exec (<service-name>) /usr/local/bin/su-exec $NETOP_USER_NAME /bin/bash";
+doPreBuild ()
+{
+    doLocalImageForDevelopment
+    doDockerComposeFile
+}
+case "$BUILD_OPTION" in
+    $COMPOSE_CACHE)
+        doPreBuild
+        doNetopCache
+        ;;
+    $COMPOSE_DB)
+        doPreBuild
+        doNetopDatabase
+        ;;
+    $COMPOSE_MQ)
+        doPreBuild
+        doNetopMq
+        ;;
+    $COMPOSE_WEB)
+        doPreBuild
+        doNetopWeb
+        ;;
+    $COMPOSE_PORTAL)
+        doPreBuild
+        doPortalWorker
+        ;;
+    $COMPOSE_NAS)
+        doPreBuild
+        doNasWorker
+        ;;
+    $COMPOSE_PERMISSIONS)
+        doPreBuild
+        doPermissionsWorker
+        ;;
+    all)
+        doPreBuild
+        doNetopCache
+        doNetopDatabase
+        doNetopMq
+        doNasWorker
+        doPortalWorker
+        doPermissionsWorker
+        doNetopWeb
+        doShowSuccessFinal
+        ;;
+    *)
+        echo "Unknown option to build";
+        exit 1;
+esac
