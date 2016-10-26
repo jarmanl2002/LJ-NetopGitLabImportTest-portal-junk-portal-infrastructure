f3d8d9a0b2a57cfc7da96e3e937f583528b000ca
Andrei Giura <giura.andrei70@gmail.com>
2016-10-26 10:32:45 +0300

    small fix

--- install.sh
+++ install.sh
@@ -1,13 +1,16 @@
+INSTALL_DIR="$(pwd)/workspace"
+mkdir workspace;
+echo $INSTALL_DIR;
+
 #!/bin/bash
 echo "Login to netop docker registry using domain credentials:"
 docker login git.netop.com:4545 || exit 1;
 
 # variables for install setup
-INSTALL_DIR="workspace"
-mkdir workspace;
+
 
 tput setaf 2;
-echo "STEP 1/3: setup workspace and clone the repos "
+echo "STEP 1/6: setup env conf "
 tput setaf 9;
 #making the workspace dirs
 mkdir -p "$INSTALL_DIR"/{portal,nas,weblogs,permissions};
@@ -22,35 +25,65 @@ cp confs/nas/app.env "$INSTALL_DIR"/nas/config/
 cp confs/nas/{env-config.js,production.js} "$INSTALL_DIR"/nas/config/env/
 cp confs/permission/app.env "$INSTALL_DIR"/permissions/config/
 
+tput setaf 2;
+echo "STEP 1/6: DONE"
+tput setaf 9;
 
 #clone git projects
+tput setaf 2;
+echo "STEP 2/6: Clone and build portal frontend"
+tput setaf 9;
 git clone git@git.netop.com:portal/netop-portal-frontend.git "$INSTALL_DIR"/portal-frontend;
 cd "$INSTALL_DIR"/portal-frontend;
 git checkout develop;
-tput setaf 2;
-echo "STEP 2/3: Build portal frontend"
-tput setaf 9;
 npm install && npm run build;
+tput setaf 2;
+echo "STEP 2/6: DONE"
+tput setaf 9;
 
+
+tput setaf 2;
+echo "STEP 3/6: Clone and build nas frontend"
+tput setaf 9;
 git clone git@git.netop.com:portal/netop-nas-frontend.git "$INSTALL_DIR"/nas-frontend;
 cd "$INSTALL_DIR"/nas-frontend;
 git checkout develop;
-tput setaf 2;
-echo "STEP 3/3: Build nas frontend"
-tput setaf 9;
 npm install && npm run build;
+tput setaf 2;
+echo "STEP 3/6: DONE"
+tput setaf 9;
 
+
+tput setaf 2;
+echo "STEP 4/6: Clone portal"
+tput setaf 9;
 git clone git@git.netop.com:portal/netop-portal-server.git "$INSTALL_DIR"/portal/project;
 cd "$INSTALL_DIR"/portal/project;
 git checkout develop;
+tput setaf 2;
+echo "STEP 4/6: DONE"
+tput setaf 9;
 
+tput setaf 2;
+echo "STEP 5/6: Clone nas"
+tput setaf 9;
 git clone git@git.netop.com:portal/netop-nas.git "$INSTALL_DIR"/nas/project;
 cd "$INSTALL_DIR"/nas/project;
 git checkout develop;
+tput setaf 2;
+echo "STEP 5/6: DONE"
+tput setaf 9;
 
+tput setaf 2;
+echo "STEP 6/6: Clone permissions"
+tput setaf 9;
 git clone git@git.netop.com:portal/netop-permissions.git "$INSTALL_DIR"/permissions/project;
 cd "$INSTALL_DIR"/permissions/project;
 git checkout develop;
+tput setaf 2;
+echo "STEP 6/6: DONE"
+tput setaf 9;
+
 
 tput setaf 2;
 echo "DONE! Your environment has been cloned in the workspace folder."
